f192b25bf771e3649191f2c2afa5df2877fb9742
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2025-12-08 13:27:32 +0530

    Changes Added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -58,69 +58,6 @@ namespace AssetRegistry.Controllers
             return Unauthorized();
         }
 
-        [HttpPost]
-        [Route("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDTO model)
-        {
-            var userExists = await _userManager.FindByNameAsync(model.Email);
-            if (userExists != null)
-                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User already exists!" });
-
-            ApplicationUser user = new()
-            {
-                Email = model.Email,
-                SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = model.Email
-            };
-            var result = await _userManager.CreateAsync(user, model.Password);
-            if (!result.Succeeded)
-                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User creation failed! Please check user details and try again." });
-
-            return Ok(new ResponseDTO { Status = "Success", Message = "User created successfully!" });
-        }
-
-        [HttpPost]
-        [Route("register-admin")]
-        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterDTO model)
-        {
-            try
-            {
-                var userExists = await _userManager.FindByNameAsync(model.Email);
-                if (userExists != null)
-                    return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User already exists!" });
-
-                ApplicationUser user = new()
-                {
-                    FirstName = model.Email,
-                    LastName = model.LastName,
-                    Nic = model.Nic,
-                    Address = model.Address,
-                    Email = model.Email,
-                    UserName = model.Email,
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    IsActive = true,
-                    CreatedBy = "Admin",
-                    CreatedOn = DateTime.Now
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
-                if (!result.Succeeded)
-                    return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User creation failed! Please check user details and try again." });
-
-                if (!await _roleManager.RoleExistsAsync("SuperAdmin"))
-                    await _roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
-
-                if (await _roleManager.RoleExistsAsync("SuperAdmin"))
-                {
-                    await _userManager.AddToRoleAsync(user, "SuperAdmin");
-                }
-                return Ok(new ResponseDTO { Status = "Success", Message = "User created successfully!" });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-
         private JwtSecurityToken GetToken(List<Claim> authClaims)
         {
             var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AssetRegistry.DTOs;
+using AssetRegistry.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AssetRegistry.Controllers
@@ -7,5 +9,81 @@ namespace AssetRegistry.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public UserController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Email);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User already exists!" });
+
+            ApplicationUser user = new()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            return Ok(new ResponseDTO { Status = "Success", Message = "User created successfully!" });
+        }
+
+        [HttpPost]
+        [Route("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] RegisterDTO model)
+        {
+            try
+            {
+                var userExists = await _userManager.FindByNameAsync(model.Email);
+                if (userExists != null)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User already exists!" });
+
+                ApplicationUser user = new()
+                {
+                    FirstName = model.Email,
+                    LastName = model.LastName,
+                    Nic = model.Nic,
+                    Address = model.Address,
+                    Email = model.Email,
+                    UserName = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    IsActive = true,
+                    CreatedBy = "Admin",
+                    CreatedOn = DateTime.Now
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (!result.Succeeded)
+                    return StatusCode(StatusCodes.Status500InternalServerError, new ResponseDTO { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+                if (!await _roleManager.RoleExistsAsync("SuperAdmin"))
+                    await _roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
+
+                if (await _roleManager.RoleExistsAsync("SuperAdmin"))
+                {
+                    await _userManager.AddToRoleAsync(user, "SuperAdmin");
+                }
+                return Ok(new ResponseDTO { Status = "Success", Message = "User created successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
     }
 }
