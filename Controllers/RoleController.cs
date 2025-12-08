7e292dc2f207275a9ed03ce1fa0dd46205552491
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2025-12-08 12:59:15 +0530

    Changes Added

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using AssetRegistry.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetRegistry.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public RoleController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateAsync(string roleName)
+        {
+            var _res = await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
+            return Ok();
+        }
+    }
+}
--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using AssetRegistry.Models.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Newtonsoft.Json;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -18,47 +19,6 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        ChangeTracker.DetectChanges();
-
-        var modifiedEntities = ChangeTracker.Entries();
-        var logList = new List<DataTransactionLog>();
-
-        foreach (var change in modifiedEntities)
-        {
-            var state = change.State;
-            if (state == EntityState.Added || state == EntityState.Modified || state == EntityState.Deleted)
-            {
-                var entityName = change.Entity.GetType().Name;
-                var currentValues = change.CurrentValues;
-                var originalValues = change.OriginalValues;
-
-                var log = new DataTransactionLog
-                {
-                    EntityName = entityName,
-                    UserName = _httpContextAccessor?.HttpContext?.User?.Identity?.Name,
-                    TransactionType = state.ToString(),
-                    TransactionDate = DateTime.Now
-                };
-
-                var logDetails = new List<TransactionLogDetail>();
-                foreach (var property in currentValues.Properties)
-                {
-                    logDetails.Add(new TransactionLogDetail
-                    {
-                        PropertyName = property.Name,
-                        CurrentValue = currentValues[property.Name]?.ToString(),
-                        OriginalValue = originalValues[property.Name]?.ToString()
-                    });
-                }
-
-                log.LogDetails = JsonConvert.SerializeObject(logDetails);
-                logList.Add(log);
-            }
-        }
-
-        if (logList.Any())
-            await base.AddRangeAsync(logList, cancellationToken);
-
         return await base.SaveChangesAsync(cancellationToken);
     }
 
